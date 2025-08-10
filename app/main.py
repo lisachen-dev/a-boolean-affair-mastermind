af4f5323d6c05fde2f061dd9659475438f2c7b4b
Lisa Chen <lisa.hl.chen@gmail.com>
2025-08-10 12:13:56 -0700

    refactor: update logger to read from .env and output custom format

--- app/main.py
+++ app/main.py
@@ -1,13 +1,18 @@
 import logging
+import os
 
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from starlette.responses import RedirectResponse
 
+load_dotenv()
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
 logger = logging.getLogger(__name__)
 logging.basicConfig(
-	level=logging.DEBUG
-)  # DEBUG, INFO, WARNING, ERROR, CRITICAL (order of filtered views)
-logger.info("-------You have now started A Boolean Affair!")
+	level=getattr(logging, LOG_LEVEL, logging.INFO),
+	format="%(asctime)s [%(levelname)s - %(name)s] --- %(message)s",
+)
+logger.info("You have now started A Boolean Affair!")
 
 app = FastAPI(title="A Boolean Affair")
 
@@ -15,5 +20,5 @@ app = FastAPI(title="A Boolean Affair")
 # Root Route redirects to Swagger docs
 @app.get("/", include_in_schema=False)
 async def redirect_to_docs():
-	logger.info("-------Redirecting to Swagger UI")
+	logger.info("Redirecting to Swagger UI")
 	return RedirectResponse(url="/docs")
