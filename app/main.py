452a48fb224bf3aafa2ed97c851bbf5d9139dace
Lisa Chen <lisa.hl.chen@gmail.com>
2025-08-09 20:29:16 -0700

    feat: add logging setup and initial DEBUG/INFO messages

--- app/main.py
+++ app/main.py
@@ -1,15 +1,19 @@
-from fastapi import FastAPI
-from starlette.responses import RedirectResponse
 import logging
 
-logger = logging.getLogger('uvicorn.error')
-logger.setLevel(logging.DEBUG)
+from fastapi import FastAPI
+from starlette.responses import RedirectResponse
 
-app = FastAPI()
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+	level=logging.DEBUG
+)  # DEBUG, INFO, WARNING, ERROR, CRITICAL (order of filtered views)
+logger.info("-------You have now started A Boolean Affair!")
+
+app = FastAPI(title="A Boolean Affair")
 
 
 # Root Route redirects to Swagger docs
 @app.get("/", include_in_schema=False)
 async def redirect_to_docs():
-	logger.debug('this is a debug message')
+	logger.info("-------Redirecting to Swagger UI")
 	return RedirectResponse(url="/docs")
