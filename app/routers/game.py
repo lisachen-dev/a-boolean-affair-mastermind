a3a83b7b7806f3e1530db31f79669275f4f36107
Lisa Chen <lisa.hl.chen@gmail.com>
2025-08-13 06:00:31 -0700

    refactor: reference correct mapper location

--- app/routers/game.py
+++ app/routers/game.py
@@ -5,6 +5,7 @@ from fastapi import APIRouter
 
 from app.app_context import game_service
 from app.models.game import GameCreate, GameRead
+from app.utils.mappers import to_game_read
 
 logger = logging.getLogger(__name__)
 
@@ -14,15 +15,17 @@ router = APIRouter(prefix="/games", tags=["Games"])
 @router.post("/", response_model=GameRead)
 def start_game(new_game: GameCreate):
 	game = game_service.start_game(new_game)
-	logger.debug("start_game -> %r", game)
-	return game_service.to_game_read(game=game)
+
+	return to_game_read(game=game)
 
 
 @router.get("/", response_model=list[GameRead])
 def get_all_games():
-	return game_service.get_all_games()
+	games = game_service.get_all_games()
+	return [to_game_read(game) for game in games]
 
 
 @router.get("/{game_id}", response_model=GameRead)
 def get_game(game_id: UUID) -> GameRead:
-	return game_service.get_game(game_id)
+	game = game_service.get_game(game_id)
+	return to_game_read(game)
--- app/routers/player.py
+++ app/routers/player.py
@@ -14,14 +14,13 @@ router = APIRouter(prefix="/players", tags=["Players"])
 @router.post("/", response_model=PlayerRead)
 def create_player(new_player: PlayerCreate):
 	player = player_service.create(new_player.name)
-
-	return player_service.to_player_read(player)
+	return to_player_read(player)
 
 
 @router.get("/{player_id}", response_model=PlayerRead)
 def get_player(player_id: UUID):
 	player = player_service.get(player_id=player_id)
-	return player_service.to_player_read(player)
+	return to_player_read(player)
 
 
 @router.get("/", response_model=list[PlayerRead])
