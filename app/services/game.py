0929559b48aa38f90c7f0b59808d978863bca5df
Lisa Chen <lisa.hl.chen@gmail.com>
2025-08-13 02:01:19 -0700

    feat: add guess submit, get all guesses, create helper methods, create guess methods - reorganized game service methods for readability - removed redundant logic

--- app/services/game.py
+++ app/services/game.py
@@ -1,24 +1,24 @@
 import logging
+from datetime import datetime, timezone
 from uuid import UUID
 
-from app.models.game import Game, GameCreate, GameRead
+from app.models.game import Game, GameCreate, GameRead, GameStatus
 from app.models.guess import Guess, GuessCreate, GuessRead
 from app.models.rules import Rules
 from app.repositories.game import GameStorage
 from app.services.player import PlayerService
 from app.services.random import RandomService
+from app.utils.mappers import to_game_read, to_guess_read
 from app.utils.validators import validate_code_sequence
 
 logger = logging.getLogger(__name__)
 
 """
-
 TODO:
 attempts_made == max_guesses - attempts_made or 0
 if status not IN_PROGRESS, finished_at should be set
 if status == WON, the last guess should have exact_matches exact_matches == code.length
 reject new guesses after the status is not IN_PROGRESS
-
 """
 
 
@@ -33,11 +33,7 @@ class GameService:
 		self.random_service = random_service
 		self.player_service = player_service
 
-
 	# GAME
-	@staticmethod
-	def _decide_default(value, fallback):
-		return value if value is not None else fallback
 
 	def start_game(self, game_create: GameCreate) -> Game:
 		logger.debug("Starting game: %s", game_create)
@@ -65,74 +61,96 @@ class GameService:
 
 	def get_all_games(self) -> list[GameRead]:
 		games = self.game_storage.get_all()
-		read_games = [self.to_game_read(game) for game in games]
+		read_games = [to_game_read(game) for game in games]
 		logger.info("Returning %d games", len(read_games))
 		return read_games
 
-	def create_guess(self, game_id: UUID, new_guess: GuessCreate):
-		# game
-		# validate guess
-		# guess
 	# GUESS
 
-		# apply the guess
-		# evaluate for status
-		# save the game
-		# return guess? the game?
-		pass
+	def submit_guess(self, game_id: UUID, guess_value: list[str]) -> GuessRead:
+		"""submit guess and return the read-friendly GuessRead"""
 
-	def get_all_guesses(self) -> list[Guess]:
-		pass
+		game = self.get_game(game_id)
+		if game.status != GameStatus.IN_PROGRESS:
+			raise ValueError("This game is closed! Please start a new game.")
 
-	# helpers
-
-	def _score_guess(self, secret: tuple[str,...], guess_value: list[str]) -> Guess:
-		"""
-		game: attempts_made (len(guesses)) attempts_left
-		guess: attempt_number, exact_matches, Partial_matches
-		evaluate secret compared to guess_value:
-			number in position (exact)
-			number in secret, but not in right position (partial)
-
-			secret_leftover = []
-			guess_leftover = []
-
-			loop through and match
-				* if there is an exact match at the corresponding i, increment exact matches and move on
-				* if there isn't, store both in corresponding lists and increment partial match
-
-			for remaining lists, if any guesses are in the secret list, then increment partial match
-
-			create a dictionary to hold remaining values in secret on a count
-			compare guess_value by looping and decrement the counts for secrets, but increase the partial amount.
-		"""
-		pass
-
-	def _update_status_if_completed(self, game: Game, last_guess: Guess):
-		# WIN? LOSE?
-		pass
-
-	# mappings
-	@staticmethod
-	def to_game_read(game: Game) -> GameRead:
-		return GameRead(
-			**game.model_dump(
-				include={
-					"id",
-					"status",
-					"player_id",
-					"guesses",
-					"finished_at",
-					"code_length",
-					"max_guesses",
-					"min_value",
-					"max_value",
-					"allow_repeats",
-					"created_at",
-				}
-			)
+		# validate guess input against game rules
+		validate_code_sequence(
+			values=guess_value,
+			code_length=game.code_length,
+			min_value=game.min_value,
+			max_value=game.max_value,
+			allow_repeats=game.allow_repeats,
+			label="[GUESS]",
 		)
 
+		create_model = GuessCreate(guess_value=guess_value)
+		exact_matches, partial_matches = self._score_guess(
+			secret=game._secret, guess_value=guess_value
+		)
+
+		guess = Guess(
+			game_id=game.id,
+			guess_value=create_model.guess_value,
+			exact_matches=exact_matches,
+			partial_matches=partial_matches,
+			created_at=self._utc_now(),
+		)
+
+		game.guesses.append(guess)
+
+		self._update_status_if_completed(game=game, last_guess=guess)
+		self.game_storage.update(game)
+
+		logger.info("Guess has been recorded: %s", guess.guess_value)
+		return to_guess_read(guess)
+
+	def get_all_guesses(self, game_id: UUID) -> list[Guess]:
+		game_obj = self.get_game(game_id=game_id)
+		return game_obj.guesses
+
+	# HELPERS
+
 	@staticmethod
-	def to_guess_read(game_id: UUID, guess: Guess) -> GuessRead:
-		pass
+	def create_guess(guess_value: list[str]) -> GuessCreate:
+		return GuessCreate(guess_value=guess_value)
+
+	@staticmethod
+	def _score_guess(secret: tuple[str, ...], guess_value: list[str]) -> tuple[int, int]:
+		"""
+		Returns exact_match_count and partial_match_count
+		assumes lengths have been validated
+		"""
+		# determine exact matches and store remaining values for later
+		exact_match_count = 0
+		secret_remaining = {}
+		guess_remaining = []
+
+		for i in range(len(secret)):
+			if guess_value[i] == secret[i]:
+				exact_match_count += 1
+				continue
+
+			guess_remaining.append(guess_value[i])
+			secret_remaining[secret[i]] = secret_remaining.get(secret[i], 0) + 1
+
+		# determine partial matches on remaining values without double counting
+		partial_match_count = 0
+		for guess_val in guess_remaining:
+			if secret_remaining.get(guess_val, 0) > 0:
+				partial_match_count += 1
+				secret_remaining[guess_val] -= 1
+
+		return exact_match_count, partial_match_count
+
+	def _update_status_if_completed(self, game: Game, last_guess: Guess):
+		if last_guess.exact_matches == game.code_length:
+			game.status = GameStatus.WON
+			game.finished_at = self._utc_now()
+		elif len(game.guesses) >= game.max_guesses:
+			game.status = GameStatus.LOST
+			game.finished_at = self._utc_now()
+
+	@staticmethod
+	def _utc_now():
+		return datetime.now(timezone.utc)
