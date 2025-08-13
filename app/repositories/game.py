2c37c5198103ea961a1d3ae59c889cf618cd530b
Lisa Chen <lisa.hl.chen@gmail.com>
2025-08-13 01:46:02 -0700

    feat: add update game storage method

--- app/repositories/game.py
+++ app/repositories/game.py
@@ -17,7 +17,7 @@ class GameStorage:
 
 	def get(self, game_id: UUID) -> Game:
 		if game_id not in self._games:
-			raise KeyError("Game is not found")
+			raise KeyError("Game not found")
 
 		logger.info("Found the game!")
 		return self._games[game_id]
@@ -30,5 +30,11 @@ class GameStorage:
 		for game in self._games.values():
 			if game.player_id == player_id:
 				filtered_games_by_player.append(game)
-
 		return filtered_games_by_player
+
+	def update(self, game: Game) -> Game:
+		if game not in self._games:
+			raise KeyError("Game not found")
+		self._games[game.id] = game
+		logger.info("Game updated successfully! id=%s", game.id)
+		return game
